4a79a954549cf20db2ecbf70af3fd09678f6958c
Ankur <ankur1703@yahoo.co.in>
2015-10-17 20:19:09 +0100

    First Commit

---
+++ src/MainPackage/DistanceCalculater.java
@@ -0,0 +1,21 @@
+package MainPackage;
+
+public class DistanceCalculater {
+	
+	public double calculateDistance(double latitudeOfPoint1, double longitudeOfpoint1, double latitudeOfPoint2, double longitudeOfpoint2){
+		double distance = 0d;
+		int radius = 6371;
+		
+		latitudeOfPoint1 = Math.toRadians(latitudeOfPoint1);
+		latitudeOfPoint2 = Math.toRadians(latitudeOfPoint2);	
+		double differenceBetweenLongitude = Math.toRadians(Math.abs(longitudeOfpoint1-longitudeOfpoint2));
+		
+		double angleBetweenPoints = Math.acos(Math.sin(latitudeOfPoint1)*Math.sin(latitudeOfPoint2)
+								              + Math.cos(latitudeOfPoint1)*Math.cos(latitudeOfPoint2)
+								              *Math.cos(differenceBetweenLongitude));
+		
+		distance = radius*angleBetweenPoints;
+		return distance;
+	}
+	
+}
---
+++ src/MainPackage/Tester.java
@@ -0,0 +1,11 @@
+package MainPackage;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		DistanceCalculater distanceCalculater = new DistanceCalculater();
+		
+		// Dublin office (GPS coordinates 53.3381985, -6.2592576)
+	}
+
+}
